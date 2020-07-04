969f0cc71c556f07589683b937b1417a14b8d61c
PHPBOSS1 <kkonstantinov926@gmail,com>
2020-07-04 14:01:24 +0300

    the first commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth'] ], function () {
+    Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+    Route::resource('/category', 'CategoryController', ['as'=>'admin']);
+    Route::resource('/article', 'ArticleController', ['as'=>'admin']);
+
+});
+
+Route::get('/', function () {
+    return view('blog.home');
+});
+
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::group(['middleware'	=>	'auth'], function(){
+
+    Route::get('/settings', 'SettingsController@edit')->name("settings");
+    Route::post('settings/store', "SettingsController@store");
+
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
